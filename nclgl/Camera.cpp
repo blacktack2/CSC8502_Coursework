9e776cf277dadda82839a08345dc58c2cd258a65
b9050231 <b9050231@newcastle.ac.uk>
2022-11-18 10:07:11 +0000

    Implemented keyframe based auto camera movement

--- nclgl/Camera.cpp
+++ nclgl/Camera.cpp
@@ -8,33 +8,54 @@ roll(roll_), pitch(pitch_), yaw(yaw_), position(position_) {
 }
 
 void Camera::Update(float dt) {
-	float speed = (Window::GetKeyboard()->KeyDown(KEYBOARD_CONTROL) ? 200.0f : 30.0f) * dt;
-	float rollSpeed = 30.0f * dt;
+	if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_9))
+		std::cout << position << " " << roll << " " << pitch << " " << yaw << "\n";
+	if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_0))
+		autoMove = !autoMove;
+	if (autoMove) {
+		autoTime = fmodf((autoTime + dt * AUTO_SPEED), KEY_FRAMES[KEY_FRAMES.size() - 1].time);
+		for (int i = 1; i < KEY_FRAMES.size(); i++) {
+			KeyFrame r = KEY_FRAMES[i];
+			if (autoTime < r.time) {
+				KeyFrame l = KEY_FRAMES[i - 1];
+				float t = (autoTime - l.time) / (r.time - l.time);
 
-	roll += (Window::GetKeyboard()->KeyDown(KEYBOARD_Q) ? rollSpeed : 0.0f) - (Window::GetKeyboard()->KeyDown(KEYBOARD_E) ? rollSpeed : 0.0f);
-	roll += (roll < 0.0f) ? 360.0f : ((roll >= 360.0f) ? -360.0f : 0.0f);
+				position = Vector3::Lerp(l.position, r.position, t);
+				roll = l.roll + (r.roll - l.roll) * t;
+				pitch = l.pitch + (r.pitch - l.pitch) * t;
+				yaw = l.yaw + (r.yaw - l.yaw) * t;
+				break;
+			}
+		}
+	} else {
+		float speed = (Window::GetKeyboard()->KeyDown(KEYBOARD_CONTROL) ? 200.0f : 30.0f) * dt;
+		float rollSpeed = 30.0f * dt;
 
-	pitch = std::min(std::max(pitch - Window::GetMouse()->GetRelativePosition().y, -90.0f), 90.0f);
+		roll += (Window::GetKeyboard()->KeyDown(KEYBOARD_Q) ? rollSpeed : 0.0f) - (Window::GetKeyboard()->KeyDown(KEYBOARD_E) ? rollSpeed : 0.0f);
+		roll += (roll < 0.0f) ? 360.0f : ((roll >= 360.0f) ? -360.0f : 0.0f);
 
-	yaw -= Window::GetMouse()->GetRelativePosition().x;
-	yaw += (yaw < 0.0f) ? 360.0f : ((yaw >= 360.0f) ? -360.0f : 0.0f);
+		pitch = std::min(std::max(pitch - Window::GetMouse()->GetRelativePosition().y, -90.0f), 90.0f);
 
-	Matrix4 rotation = Matrix4::Rotation(yaw, Vector3(0.0f, 1.0f, 0.0f));
-	Vector3 forward = rotation * Vector3(0.0f, 0.0f, -1.0f);
-	Vector3 right = rotation * Vector3(1.0f, 0.0f, 0.0f);
+		yaw -= Window::GetMouse()->GetRelativePosition().x;
+		yaw += (yaw < 0.0f) ? 360.0f : ((yaw >= 360.0f) ? -360.0f : 0.0f);
 
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_W))
-		position += forward * speed;
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_S))
-		position -= forward * speed;
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_A))
-		position -= right * speed;
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_D))
-		position += right * speed;
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_SPACE))
-		position.y += speed;
-	if (Window::GetKeyboard()->KeyDown(KEYBOARD_SHIFT))
-		position.y -= speed;
+		Matrix4 rotation = Matrix4::Rotation(yaw, Vector3(0.0f, 1.0f, 0.0f));
+		Vector3 forward = rotation * Vector3(0.0f, 0.0f, -1.0f);
+		Vector3 right = rotation * Vector3(1.0f, 0.0f, 0.0f);
+
+		if (Window::GetKeyboard()->KeyDown(KEYBOARD_W))
+			position += forward * speed;
+		if (Window::GetKeyboard()->KeyDown(KEYBOARD_S))
+			position -= forward * speed;
+		if (Window::GetKeyboard()->KeyDown(KEYBOARD_A))
+			position -= right * speed;
+		if (Window::GetKeyboard()->KeyDown(KEYBOARD_D))
+			position += right * speed;
+		if (Window::GetKeyboard()->KeyDown(KEYBOARD_SPACE))
+			position.y += speed;
+		if (Window::GetKeyboard()->KeyDown(KEYBOARD_SHIFT))
+			position.y -= speed;
+	}
 }
 
 Matrix4 Camera::BuildViewMatrix() {
--- nclgl/Camera.h
+++ nclgl/Camera.h
@@ -2,6 +2,8 @@
 #include "Matrix3.h"
 #include "Matrix4.h"
 
+#include <vector>
+
 class Camera {
 public:
 	Camera(float roll = 0.0f, float pitch = 0.0f, float yaw = 0.0f, Vector3 position = Vector3(0.0f, 0.0f, 0.0f));
@@ -28,4 +30,32 @@ protected:
 	float yaw;
 
 	Vector3 position;
+
+	bool autoMove = true;
+	float autoTime = 0.0f;
+
+	const float AUTO_SPEED = 2.0f;
+
+	struct KeyFrame {
+		float time;
+		Vector3 position;
+		float roll;
+		float pitch;
+		float yaw;
+	};
+	const std::vector<KeyFrame> KEY_FRAMES = std::vector<KeyFrame>{
+		{0.0f, Vector3(-3.0f, 56.0f, 21.0f), 0.0f, 21.0f, -20.0f},
+		{3.0f, Vector3(14.0f, 80.0f, -14.0f), 0.0f, 17.0f, -20.0f},
+		{4.0f, Vector3(19.0f, 80.0f, -30.0f), 0.0f, -2.0f, -10.0f},
+		{6.0f, Vector3(13.0f, 74.0f, -62.0f), 0.0f, -11.0f, 25.0f},
+		{7.0f, Vector3(-11.0f, 72.0f, -101.0f), 0.0f, -10.0f, 34.0f},
+		{9.0f, Vector3(-41.0f, 56.0f, -130.0f), 0.0f, -17.0f, 57.0f},
+		{13.0f, Vector3(-94.0f, 40.0f, -152.0f), 0.0f, -1.0f, 73.0f},
+		{15.0f, Vector3(-115.0f, 52.0f, -157.0f), 0.0f, 27.0f, 76.0f},
+		{17.0f, Vector3(-229.0f, 72.0f, -186.0f), 0.0f, -7.0f, 71.0f},
+		{24.0f, Vector3(-326.0f, 72.0f, -248.0f), 0.0f, -4.0f, 51.0f},
+		{30.0f, Vector3(-421.0f, 107.0f, -281.0f), 0.0f, -3.0f, 83.0f},
+		{50.0f, Vector3(-1553.0f, 107.0f, 205.0f), 0.0f, 30.0f, 95.0f},
+		{70.0f, Vector3(-2523.0f, 107.0f, -567.0f), 0.0f, -7.0f, 68.0f},
+	};
 };
